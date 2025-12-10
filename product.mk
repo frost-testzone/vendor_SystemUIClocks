37f3e10836b81e84c01cdc97480ac1830fedb708
Frost <frost.github@proton.me>
2025-12-10 15:24:10 +0000

    SystemUIClocks: Update from mustang BP4A.251205.006
    Change-Id: I8e4d34113bb63db47a0ec39cd54809075d057121
    Signed-off-by: Frost <frost.github@proton.me>

--- product.mk
+++ product.mk
@@ -5,7 +5,6 @@ PRODUCT_SOONG_NAMESPACES += \
 PRODUCT_PACKAGES += \
     SystemUIClocks-BigNum \
     SystemUIClocks-Calligraphy \
-    SystemUIClocks-Flex \
     SystemUIClocks-Growth \
     SystemUIClocks-Inflate \
     SystemUIClocks-Metro \
